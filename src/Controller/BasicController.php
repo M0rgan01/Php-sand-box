9163f3353a3e328abf0c14136738350096089a80
PICHAT Morgan <pichat.morgan@gmail.com>
2023-05-25 18:47:49 +0200

    Add twig

--- src/Controller/BasicController.php
+++ src/Controller/BasicController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class BasicController
+class BasicController extends AbstractController
 {
     #[Route('/basic/number/{max}', name: 'app_lucky_number')]
     public function number(int $max): Response
@@ -15,5 +17,10 @@ class BasicController
         return new Response(
             '<html><body>Lucky number: ' . $number . '</body></html>'
         );
+    }
+
+    public function index(): Response
+    {
+        return new Response($this->render('pages/home.html.twig'));
     }
 }
